1278c00a722ce2875c514bb1155875d7060207f1
jian-kowalski <jian_kowalski@hotmail.com>
2021-03-17 21:30:43 -0300

    Adição do balancemento de carga com ribbon

--- hr-payroll/src/main/java/com/jiankowalski/hrpayroll/domain/feignclients/WorkerFeignClients.java
+++ hr-payroll/src/main/java/com/jiankowalski/hrpayroll/domain/feignclients/WorkerFeignClients.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "hr-worker", url = "127.0.0.1:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClients {
 
     @GetMapping("/{workerId}")
--- hr-worker/src/main/java/com/jiankowalski/hrworker/api/controller/WorkerController.java
+++ hr-worker/src/main/java/com/jiankowalski/hrworker/api/controller/WorkerController.java
@@ -3,6 +3,10 @@ package com.jiankowalski.hrworker.api.controller;
 import com.jiankowalski.hrworker.api.Model.WorkerModel;
 import com.jiankowalski.hrworker.api.assembler.WorkerAssembler;
 import com.jiankowalski.hrworker.domain.service.WorkerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +18,16 @@ import java.util.List;
 @RequestMapping("/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    private final Environment env;
     private final WorkerService workerService;
     private final WorkerAssembler workerAssembler;
-
-    public WorkerController(WorkerService workerService, WorkerAssembler workerAssembler) {
+    
+    public WorkerController(WorkerService workerService, WorkerAssembler workerAssembler, Environment env) {
         this.workerService = workerService;
         this.workerAssembler = workerAssembler;
+        this.env = env;
     }
 
     @GetMapping
@@ -29,6 +37,7 @@ public class WorkerController {
 
     @GetMapping("/{workerId}")
     public WorkerModel findById(@PathVariable Long workerId){
+        logger.info(String.format("PORT = %s", env.getProperty("local.server.port")));
         return workerAssembler.toModel(workerService.findWorkerByID(workerId));
     }
 
